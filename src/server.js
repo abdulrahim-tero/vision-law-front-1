fd79c22ec6d3bb8e9d9147327da2ef9dad94976a
abdulrahim Terro <abdulrahim.terro@gmail.com>
2022-12-11 16:06:18 +0400

    .

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 app.use(cors({ origin: '*' }));
-
-app.use('*', express.static(path.join(process.cwd(), '/index.html')));
+console.log(path.join(process.cwd(), 'build'));
+app.use('*', express.static(path.join(process.cwd(), 'build')));
 
 const httpServer = require('http').createServer(app);
 
